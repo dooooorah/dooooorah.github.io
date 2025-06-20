29b2f3182f3c82918106ced18a1e92ca53be0010
dooooorah <mohidrashid91326@gmail.com>
2025-06-19 20:54:31 -0700

    Add: Defined about me elements in jsx file

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,6 @@
 import Card from '../components/DirectoryCard.jsx'
 import './home.css'
-import hi from '../assets/photos/hi.jpg'
+import hi from '../assets/photos/hi.png'
 
 export default function Home()
 {
@@ -9,13 +9,15 @@ export default function Home()
             <div id="background">
                 <h2 class="title">Welcome to my Portfolio!</h2>
 
+
                 <div id='homebody'>
                     <p id="bodytext">I'm Mohid, a student in Computing Science at Simon Fraser University, and this 
                         website is a project I worked on for a course I took called CMPT 276 : Intro to Software Engineering.
-                        It contains some basic information about who I am, my professional experience, my story with programming, and 
-                        where I believe I am with some of the skills I'm learning!
+                        It contains some basic information about who I am in terms of hobbies, family, and my current life!
+                        It also includes my professional experience, my story with programming, and 
+                        where I believe I am with some of the skills I'm learning.
                         <br></br><br></br>
-                        I hope you'll find my site interesting as I worked really hard on it...
+                        I hope you'll find my site interesting as I worked really hard on it.
                     </p>
 
                     <img src={hi} id="imgHi"></img>
