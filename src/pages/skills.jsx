d90c538533fce451d8a9dc92b93ec1c9d3fc4f85
dooooorah <mohidrashid91326@gmail.com>
2025-06-19 23:34:23 -0700

    Styling: Styled skill meter for functionality

--- src/pages/skills.jsx
+++ src/pages/skills.jsx
@@ -6,6 +6,8 @@ export default function Skills()
     return (
         <>
             <div id="background">
+                <h2 id="title">Skill Meter</h2>
+                <p>Click each button to see how confident I am in each skill!</p>
                 <div className="skill-wrapper">
                     <Meter />
                 </div>
