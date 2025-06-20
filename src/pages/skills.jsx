809ed70d0ab53fe70624072cf61da5086bed3853
dooooorah <mohidrashid91326@gmail.com>
2025-06-19 23:40:54 -0700

    Add: styled skills page slightly and added directory

--- src/pages/skills.jsx
+++ src/pages/skills.jsx
@@ -1,5 +1,6 @@
 import Meter from '../components/meter.jsx';
-
+import Card from '../components/DirectoryCard.jsx'
+import './skills.css'
 
 export default function Skills()
 {
@@ -11,7 +12,21 @@ export default function Skills()
                 <div className="skill-wrapper">
                     <Meter />
                 </div>
+                
+                
+                <h2>You can also check out:</h2>
+                <nav className="sknav">
+                    <Card page="/about-me" source="" altText="About me page" title="About me"/>
+                    <Card page="/experience" source="" altText="Experience page" title="Experience"/>
+                    <Card page="/my-story" source="" altText="My Story page" title="My Story"/>
+                </nav>
+
+                <h2>Or, you can go back to home:</h2>
+                <nav className="sknav">
+                    <Card page="/" source="" altText="Home page" title="Home"/>
+                </nav>
             </div>
+
         </>
     )
 }
